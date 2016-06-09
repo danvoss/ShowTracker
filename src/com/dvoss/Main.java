ce1e456e354bc12d10a68f136b05fde37f855de6
Dan <danielandrewvoss@gmail.com>
2016-06-09 14:51:34 -0400

    Add show display

--- src/com/dvoss/Main.java
+++ src/com/dvoss/Main.java
@@ -15,6 +15,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        addTestShows();
+
         Spark.init();
         Spark.get(
                 "/",
@@ -65,7 +67,8 @@ public class Main {
                     return "";
                 }
         );
-
-
+    }
+    static void addTestShows(){
+        shows.add(new Show("me", "Prince", "Jan 1", "NYC", "great!"));
     }
 }
