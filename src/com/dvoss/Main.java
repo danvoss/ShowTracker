4fb4fabd7df9910058e4254ed449182586ebe4df
Dan <danielandrewvoss@gmail.com>
2016-06-09 14:33:32 -0400

    Add login/logout

--- src/com/dvoss/Main.java
+++ src/com/dvoss/Main.java
@@ -1,5 +1,10 @@
 package com.dvoss;
 
+import spark.ModelAndView;
+import spark.Session;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -9,6 +14,58 @@ public class Main {
     static ArrayList<Show> shows = new ArrayList<>();
 
     public static void main(String[] args) {
-	// write your code here
+
+        Spark.init();
+        Spark.get(
+                "/",
+                (request, response) -> {
+                    Session session = request.session();
+
+                    String username = session.attribute("username");
+                    String pw = session.attribute("pass");
+
+                    HashMap m = new HashMap();
+                    m.put("shows", shows);
+                    m.put("username", username);
+                    m.put("pass", pw);
+                    return new ModelAndView(m, "home.html");
+                },
+                new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/login",
+                (request, response) -> {
+
+                    String username = request.queryParams("username");
+                    String pw = request.queryParams("pass");
+                    if (username == null || pw == null) {
+                        Spark.halt("Name or password not sent");
+                    }
+                    User user = users.get(username);
+                    if (user == null) {
+                        user = new User(username, pw);
+                        users.put(username, user);
+                    }
+                    else if (!pw.equals(user.password)) {
+                        Spark.halt("Wrong password");
+                    }
+                    Session session = request.session();
+                    session.attribute("username", username);
+
+                    response.redirect(request.headers("Referer"));
+                    return "";
+                }
+        );
+        Spark.post(
+                "/logout",
+                (request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
+                    response.redirect("/");
+                    return "";
+                }
+        );
+
+
     }
 }
--- src/com/dvoss/Show.java
+++ src/com/dvoss/Show.java
@@ -4,12 +4,14 @@ package com.dvoss;
  * Created by Dan on 6/9/16.
  */
 public class Show {
+    String creator;
     String artist;
     String date;
     String location;
     String notes;
 
-    public Show(String artist, String date, String location, String notes) {
+    public Show(String creator, String artist, String date, String location, String notes) {
+        this.creator = creator;
         this.artist = artist;
         this.date = date;
         this.location = location;
