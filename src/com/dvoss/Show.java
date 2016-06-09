fd46751683ea7821b41cc7836c9358d466ad3797
Dan <danielandrewvoss@gmail.com>
2016-06-09 13:24:54 -0400

    Initial import

---
+++ src/com/dvoss/Show.java
@@ -0,0 +1,18 @@
+package com.dvoss;
+
+/**
+ * Created by Dan on 6/9/16.
+ */
+public class Show {
+    String artist;
+    String date;
+    String location;
+    String notes;
+
+    public Show(String artist, String date, String location, String notes) {
+        this.artist = artist;
+        this.date = date;
+        this.location = location;
+        this.notes = notes;
+    }
+}
---
+++ src/com/dvoss/User.java
@@ -0,0 +1,14 @@
+package com.dvoss;
+
+/**
+ * Created by Dan on 6/9/16.
+ */
+public class User {
+    String name;
+    String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
