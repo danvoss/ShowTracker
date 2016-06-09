e96b68a4036126ac772878786b5515f232e309fc
Dan <danielandrewvoss@gmail.com>
2016-06-09 13:31:22 -0400

    Create user hashmap and shows arraylist

--- src/com/dvoss/Main.java
+++ src/com/dvoss/Main.java
@@ -1,6 +1,12 @@
 package com.dvoss;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
+
+    static HashMap<String, User> users = new HashMap<>();
+    static ArrayList<Show> shows = new ArrayList<>();
 
     public static void main(String[] args) {
 	// write your code here
